951df9810aa29defe2763630bf0d90005948c1e9
Lamo G <lakshayg@gmail.com>
2021-11-23 14:52:09 +0000

    cont methods

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,24 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show]
+
+  def index
+    if params[:query].present?
+      @exercises = Exercise.search_by_name_category_sport_and_difficulty(params[:query])
+    else
+      @exercises = Exercise.all
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
+  end
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :category, :sport, :difficulty, :description, :muscle_group, :exercise_image)
+  end
 end
