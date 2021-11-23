c0958b628bc78ee4b571aa5e9eb038840c6ad2c1
Oliver Barnes <oliverbarnes124@gmail.com>
2021-11-23 15:45:11 +0000

    did workout methods

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,26 @@
 class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    @user = current_user
+    @workout.user = @user
+    if @workout.save
+      redirect_to @workout
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name, :category, :sport, :difficulty, :workout_duration, :equipment, :muscle_group, :description)
+  end
 end
