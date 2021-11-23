8deb202703611104f4beee0645abaa3618f50a15
Oliver Barnes <oliverbarnes124@gmail.com>
2021-11-23 17:01:19 +0000

    added controller methods - views and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.new(
+  email: "gym@bro.roidz",
+  password: "123123"
+)
+puts "saving"
+user1.save!
+puts "saved"
+
+##############################################################
+
+workout = Workout.new(
+  name: "Sweaty warmup!",
+  category: "warmup",
+  sport: "skiing",
+  difficulty: "🥵",
+  workout_duration: "30",
+  equipment: "Nothing needed for this one!",
+  muscle_group: "quads",
+  description: "very tough, you  will be sweating like cray cray",
+  user_id: user1.id
+)
+puts "saving"
+workout.save!
+puts "saved"
+
+workout = Workout.new(
+  name: "Relaxy laxative",
+  category: "weights",
+  sport: "shitting",
+  difficulty: "😴",
+  workout_duration: "10",
+  equipment: "Nothing needed for this one!",
+  muscle_group: "glutes",
+  description: "if you want a good stretch before a tough shit I have the Warmup for YOU!",
+  user_id: user1.id
+)
+puts "saving"
+workout.save!
+puts "saved"
